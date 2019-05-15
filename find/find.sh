5a1188bff8198951581da121a983ba4c3e096a9f
sudingquan <1151015256@qq.com>
2019-05-16 00:58:37 +0800

    modify find.sh

--- find/find.sh
+++ find/find.sh
@@ -1,30 +1,38 @@
 #!/bin/bash
-nowpath=`pwd`
 function openfile() {
-    local max=0
     for i in `cat $1 | tr -c -s "a-zA-Z" " "`; do
         if [[ ${#i} -gt max ]]; then
             max=${#i}
-            local max_string=${i}
+            max_string=${i}
         fi
     done
-    printf "the string is %s, len = %d\n" ${max_string:=NULL} $max
+    printf "openfile %s\n" $1
     return
 }
 function opendir() {
+    printf "opendir %s\n" $1
     for i in `ls -A $1`; do
-        if [[ -d ${i} ]]; then
+        if [[ -d $1/${i} ]]; then
             opendir $1/${i}
-        elif [[ -f ${i} ]]; then
+        elif [[ -f $1/${i} ]]; then
             openfile $1/${i}
         fi
     done
     return
 }
-if [[ -f ${1} ]]; then
-    openfile $nowpath/${i}
-elif [[ -d ${1} ]]; then
-    opendir $nowpath/${i}
-fi
 
-cd $nowpath
+if [[ $# -eq 0 ]]; then
+    opendir "."
+    printf "the string is %s, len = %d\n" ${max_string:=NULL} $max
+else
+    for i in $@; do
+        max=0
+        max_string=0
+        if [[ -f ${i} ]];then
+            openfile ${i}
+        elif [[ -d ${i} ]]; then
+            opendir ${i}
+        fi
+    printf "the string is %s, len = %d\n" ${max_string:=NULL} $max
+    done
+fi
