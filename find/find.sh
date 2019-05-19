bc36760560b5b15c00413df55a102ab21b50f260
sudingquan <1151015256@qq.com>
2019-05-20 00:49:35 +0800

    modify find.sh

--- find/find.sh
+++ find/find.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+function Fliter() {
+    file $1 | grep -q -w "text"
+    if [[ $? -eq 0 ]];then
+        echo 0
+    else 
+        echo 1
+    fi
+}
 function openfile() {
     for i in `cat $1 | tr -c -s "a-zA-Z" " "`; do
         if [[ ${#i} -gt max ]]; then
@@ -15,7 +23,10 @@ function opendir() {
         if [[ -d $1/${i} ]]; then
             opendir $1/${i}
         elif [[ -f $1/${i} ]]; then
-            openfile $1/${i}
+            check=`Fliter $1/${i}`
+            if [[ check -eq 0 ]]; then
+                openfile $1/${i}
+            fi
         fi
     done
     return
@@ -26,10 +37,29 @@ if [[ $# -eq 0 ]]; then
     printf "the string is %s, len = %d\n" ${max_string:=NULL} $max
 else
     for i in $@; do
+        if [[ -d ${i} ]]; then
+            continue
+        elif [[ -f ${i} ]]; then
+            check=`Fliter ${i}`
+            if [[ check -eq 0 ]]; then
+                continue
+            else
+                printf "Error : %s is not a text file\n" ${i}
+                exit 1
+            fi
+        else
+            printf "Error : no such file or directory: %s\n" ${i}
+            exit 1
+        fi
+    done
+    for i in $@; do
         max=0
         max_string=0
-        if [[ -f ${i} ]];then
-            openfile ${i}
+        if [[ -f ${i} ]]; then
+            check=`Fliter ${i}`
+            if [[ check -eq 0 ]]; then
+                openfile ${i}
+            fi
         elif [[ -d ${i} ]]; then
             opendir ${i}
         fi
