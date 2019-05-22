dcd57f1d5306a082f5479fa6d72c7d4044785620
sudingquan <1151015256@qq.com>
2019-05-22 13:35:06 +0800

    modify find.sh

--- find/find.sh
+++ find/find.sh
@@ -19,22 +19,31 @@ function openfile() {
 }
 function opendir() {
     printf "opendir %s\n" $1
-    for i in `ls -A $1`; do
-        if [[ -d $1/${i} ]]; then
-            opendir $1/${i}
-        elif [[ -f $1/${i} ]]; then
-            check=`Fliter $1/${i}`
-            if [[ check -eq 0 ]]; then
-                openfile $1/${i}
+    ls -A $1 1>/dev/null 2>/dev/null
+    if [[ $? -eq  0 ]];then 
+        for i in `ls -A $1`; do
+            if [[ -d $1/${i} ]]; then
+                ls -A $1/${i} 1>/dev/null 2>/dev/null
+                if [[ $? -eq 0 ]]; then
+                    opendir $1/${i}
+                else
+                    printf "\033[31m%s : Permission denied !\033[0m\n" $1/${i}
+                fi
+            elif [[ -f $1/${i} ]]; then
+                check=`Fliter $1/${i}`
+                if [[ ${check} -eq 0 ]]; then
+                    openfile $1/${i}
+                fi
             fi
-        fi
-    done
+        done
+    fi
     return
 }
 
+printf "\t\t\t\033[1;5;37;42m\t\t开始查找\t\t\033[0m\n\n\n\n"
 if [[ $# -eq 0 ]]; then
     opendir "."
-    printf "the string is %s, len = %d\n" ${max_string:=NULL} $max
+    printf "\033[32mthe string is %s, len = %d\033[0m\n" ${max_string:=NULL} $max
 else
     for i in $@; do
         if [[ -d ${i} ]]; then
@@ -44,11 +53,11 @@ else
             if [[ check -eq 0 ]]; then
                 continue
             else
-                printf "Error : %s is not a text file\n" ${i}
+                printf "\033[31mError : %s is not a text file\033[0m\n" ${i}
                 exit 1
             fi
         else
-            printf "Error : no such file or directory: %s\n" ${i}
+            printf "\033[31mError : no such file or directory: %s\033[0m\n" ${i}
             exit 1
         fi
     done
@@ -61,8 +70,13 @@ else
                 openfile ${i}
             fi
         elif [[ -d ${i} ]]; then
-            opendir ${i}
+            ls -A ${i} 1>/dev/null 2>/dev/null
+            if [[ $? -eq 0 ]]; then
+                opendir ${i}
+            else
+                printf "\033[31m%s : Permission denied !\033[0m\n" ${i}
+            fi
         fi
-    printf "the string is %s, len = %d\n" ${max_string:=NULL} $max
+    printf "\033[32mthe string is %s, len = %d\033[0m\n" ${max_string:=NULL} $max
     done
 fi
