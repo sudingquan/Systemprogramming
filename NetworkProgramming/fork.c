fc4c13e382f1635c7118c43a74b710459ec3c301
sudingquan <1151015256@qq.com>
2019-05-28 23:03:46 +0800

    new folder

---
+++ NetworkProgramming/fork.c
@@ -0,0 +1,25 @@
+/*************************************************************************
+	> File Name: fork.c
+	> Author: sudingquan
+	> Mail: 1151015256@qq.com
+	> Created Time: 日  5/26 17:09:35 2019
+ ************************************************************************/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+    pid_t pid;
+    int x = 0;
+    for (int i = 0; i < 9; i++) {
+        pid=fork();
+        x = i++;
+        if (pid == 0) break;
+    }
+    if (pid != 0) x++;
+    printf("%d\n", x);
+    fflush(stdout);
+    sleep(10000);
+    return 0;
+}
