cb51079c8c000ce18522533ac138bbeccbeeb9c6
sudingquan <1151015256@qq.com>
2019-06-19 22:00:52 +0800

    send file

---
+++ NetworkProgramming/recv_file.c
@@ -0,0 +1,114 @@
+/*************************************************************************
+	> File Name: tcp_server.c
+	> Author: sudingquan
+	> Mail: 1151015256@qq.com
+	> Created Time: 二  6/ 4 22:21:40 2019
+ ************************************************************************/
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pwd.h>
+#include <sys/wait.h>
+#define PORT 8888
+#define MAX_SIZE 1024
+
+int creat_listen_socket() {
+    int listen_socket;
+    if ((listen_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket() error");
+        return -1;
+    }
+    struct sockaddr_in my_addr;
+    memset(&my_addr, 0, sizeof(my_addr));
+    my_addr.sin_family = AF_INET;
+    my_addr.sin_port = htons(PORT);
+    my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    if (bind(listen_socket, (struct sockaddr *)&my_addr, sizeof(my_addr)) < 0) {
+        perror("bind");
+        return -1;
+    }
+    if (listen(listen_socket, 5) < 0) {
+        perror("listen");
+    }
+    return listen_socket;
+}
+
+int wait_client(int listen_socket) {
+    struct sockaddr_in client_addr;
+    printf("wait for client...\n");
+    unsigned int addrlen = sizeof(client_addr);
+    int client_socket = accept(listen_socket, (struct sockaddr *)&client_addr, &addrlen);
+    if (client_socket < 0) {
+        perror("accept");
+        return -1;
+    }
+    printf("connection to the client %s successful\n", inet_ntoa(client_addr.sin_addr));
+    return client_socket;
+}
+
+void handler(int sig)
+{
+	
+	while (waitpid(-1,  NULL,   WNOHANG) > 0)
+	{
+		printf ("成功处理一个子进程的退出\n");
+	}
+}
+
+int main() {
+    int listen_socket = creat_listen_socket();
+    if (listen_socket < 0) {
+        exit(1);
+    }
+    while(1) {    
+        int client_socket = wait_client(listen_socket);
+        if (client_socket < 0) {
+            exit(1);
+        }
+        signal(SIGCHLD,  handler);    //处理子进程，防止僵尸进程的产生
+        int pid = fork();
+		if(pid == -1)
+		{
+			perror("fork");
+			break;
+		}
+		if(pid > 0)
+		{
+			close(client_socket);
+			continue;
+		}
+		if(pid == 0)
+		{
+			close(listen_socket);
+            char file_name[100];
+            char data[MAX_SIZE + 5];
+            recv(client_socket, file_name, 100, 0);
+            FILE *fp = NULL;
+            fp=fopen(file_name,"wb");
+            if (fp == NULL) {
+                printf("open file error\n");
+                close(client_socket);
+                break;
+            }
+            while (1) {
+                int i = recv(client_socket, data, MAX_SIZE, 0);
+                if (i == 0) {
+                    fclose(fp);
+                    fp = NULL;
+                    break;
+                }
+                fwrite(data, 1, i, fp);
+                memset(data, 0, sizeof(data));
+            }
+            close(client_socket);
+			break;
+		}
+    }
+    close(listen_socket);
+    return 0;
+}
---
+++ NetworkProgramming/send_file.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pwd.h>
+#define MAX_SIZE 1024
+
+int socket_connect(int port, char *host) {
+	int sockfd;
+	struct sockaddr_in dest_addr;
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("socket() error");
+		return -1;
+	}
+
+	memset(&dest_addr, 0, sizeof(dest_addr));
+	dest_addr.sin_family = AF_INET;
+	dest_addr.sin_port = htons(port);
+	dest_addr.sin_addr.s_addr = inet_addr(host);
+
+	//DBG("Connetion TO %s:%d\n",host,port);
+	//fflush(stdout);
+	if (connect(sockfd, (struct sockaddr *)&dest_addr, sizeof(dest_addr)) < 0) {
+		//perror("connect() error");
+		//DBG("connect() error : %s!\n", stderror(errno));
+		return -1;
+	}
+	return sockfd;
+
+}
+
+int main() {
+	int socket_fd;
+	int port = 8888;
+	//char ip_addr[20] = "39.105.82.248"; 
+	char ip_addr[20] = "192.168.2.90"; 
+    char file_name[100];
+    char data[MAX_SIZE + 5];
+    printf("Enter the file you want to send : ");
+    scanf("%s", file_name);
+    FILE *fp = NULL;
+    fp = fopen(file_name, "rb");
+    if(fp == NULL){
+        printf("open file error\n");
+        exit(1);
+    }
+	socket_fd = socket_connect(port, ip_addr);
+    if(socket_fd > 0){
+        printf("connect success!\n");
+    }
+    printf("send filename...\n");
+    send(socket_fd, file_name, 100, 0);
+    printf("send file...\n");
+    while (1) {
+        int i;
+        i = fread(data, 1, MAX_SIZE, fp);
+        send(socket_fd, data, i, 0);
+        memset(data, 0, sizeof(data));
+        if (feof(fp)) {
+            fclose(fp);
+            fp = NULL;
+            break;
+        }
+    }
+    printf("send file success\n");
+	close(socket_fd);
+	return 0;
+
+}
