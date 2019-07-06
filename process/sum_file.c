c49b426205b16923eabbdec1ec01b3db98091af7
sudingquan <1151015256@qq.com>
2019-07-06 18:29:18 +0800

    modify sum_file.c

--- process/sum_file.c
+++ process/sum_file.c
@@ -11,28 +11,42 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <sys/file.h>
-#define MAX 100
-#define PROCESS_NUM 10
+#define MAX 10000
+#define PROCESS_NUM 20
 
 char sum_data[20] = "sum_data";
 char now_data[20] = "now_data";
 
-void get_num(int *num, FILE *fp) {
+void get_num(long long *num, FILE *fp) {
     char *line = NULL;
     size_t len = 0;
 	getline(&line, &len, fp);
     fseek(fp, 0, 0);
-    *num = atoi(line);
+    *num = atoll(line);
     free(line);
 }
 
-void set_num(int num, FILE *fp) {
-	fprintf(fp, "%d", num);
+void set_num(long long num, FILE *fp) {
+	fprintf(fp, "%lld", num);
 }
 
 int main() {
     pid_t pid;
     int x = 0;
+    FILE *fp1 = fopen(sum_data, "w");
+    FILE *fp2 = fopen(now_data, "w");
+    if (fp1 == NULL) {
+        perror("fopen");
+        exit(1);
+    }
+    if (fp2 == NULL) {
+        perror("fopen");
+        exit(1);
+    }
+    set_num(0, fp1);
+    set_num(0, fp2);
+    fclose(fp1);
+    fclose(fp2);
     for (int i = 0; i < PROCESS_NUM; i++) {
         pid = fork();
         if (pid == 0) {
@@ -42,8 +56,8 @@ int main() {
     }
     if (pid == 0) {
         while (1) {
-            int sum = 0;
-            int num = 0;
+            long long sum = 0;
+            long long num = 0;
             FILE *fp1 = fopen(sum_data, "r+");
             FILE *fp2 = fopen(now_data, "r+");
             if (fp1 == NULL) {
@@ -67,8 +81,8 @@ int main() {
             num++;
             set_num(sum, fp1);
             set_num(num, fp2);
-            printf("child process %d now : %d\n", x, num);
-            printf("child process %d sum : %d\n", x, sum);
+            printf("child process %d now : %lld\n", x, num);
+            printf("child process %d sum : %lld\n", x, sum);
             fclose(fp1);
             fclose(fp2);
         }
@@ -78,13 +92,13 @@ int main() {
     while (n--) {
         waitpid(-1, NULL, 0);
     }
-    int sum;
+    long long sum;
     FILE *fp = fopen(sum_data, "r+");
     if (fp == NULL) {
         perror("fopen");
         exit(1);
     }
     get_num(&sum, fp);
-    printf("sum = %d\n", sum);
+    printf("sum = %lld\n", sum);
     return 0;
 }
